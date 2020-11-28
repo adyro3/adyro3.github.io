6884bd3962be2514222aca8e8546dccd13bf4873
adyro3 <adrianadrian198@gmail.com>
2020-11-28 15:46:20 +0200

    fbbo flower done

--- fibonacciflower.js
+++ fibonacciflower.js
@@ -1,30 +1,36 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'destination-over';
 
-let size = 0;
-let positionX = 0;
-let positionY = 0;
+let number = 0;
+let scale = 10;
+let size = 20;
+hue = Math.random() * 360;
 
 function drawFlower() {
-  ctx.fillStyle = 'red';
-  ctx.strokeStyle = 'blues';
+  let angle = number * 1;
+  let radius = scale * Math.sqrt(number);
+  let positionX = radius * Math.sin(angle) + canvas.width / 2;
+  let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%';
+  ctx.strokeStyle = 'black';
   ctx.lineWidth = 5;
   ctx.beginPath();
   ctx.arc(positionX, positionY, size, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
+
+  number++;
+  hue++;
 }
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  size += 0.05;
-  positionX += 0.5;
-  positionY += 0.5;
   drawFlower();
+  if (number > 900) return;
   requestAnimationFrame(animate);
 }
 
